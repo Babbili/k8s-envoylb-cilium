c62220ebccdd4235df4f9b3836537f5e8f6e5d0d
Babbili <babbili@protonmail.com>
2023-07-26 08:13:31 +0400

    add SCRIPT_DIR

--- goappclient/proto.sh
+++ goappclient/proto.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-protoc --go_out=proto --go_opt=paths=source_relative --go-grpc_out=proto --go-grpc_opt=paths=source_relative --proto_path=../protos/ ../protos/apps.proto
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR && protoc --go_out=proto --go_opt=paths=source_relative --go-grpc_out=proto --go-grpc_opt=paths=source_relative --proto_path=../protos/ ../protos/apps.proto
