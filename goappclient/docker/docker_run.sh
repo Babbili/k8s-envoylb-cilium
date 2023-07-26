814ddf818f8828df6431731e3ac38c18a49910ee
Babbili <babbili@protonmail.com>
2023-07-26 08:05:54 +0400

    add DOCKER_DIR

--- goappclient/docker/docker_run.sh
+++ goappclient/docker/docker_run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# docker build -f docker/Dockerfile --tag k8s-envoylb-goappclient .
+DOCKER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# cd $DOCKER_DIR && docker build -f Dockerfile --tag k8s-envoylb-goappclient ../
 
 # docker scout cves --type image k8s-envoylb-goappclient
 
 # docker run --name k8s-envoylb-goappclient-c k8s-envoylb-goappclient
 
-docker build -f docker/Dockerfile --tag babbili/k8s-envoylb-goappclient:6aea9c1d .
+cd $DOCKER_DIR && docker build -f Dockerfile --tag babbili/k8s-envoylb-goappclient:6aea9c1d ../
 
 docker scout cves --type image babbili/k8s-envoylb-goappclient:6aea9c1d
 
--- goappserver/docker/docker_run.sh
+++ goappserver/docker/docker_run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# docker build -f docker/Dockerfile --tag k8s-envoylb-goappserver .
+DOCKER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# cd $DOCKER_DIR && docker build -f Dockerfile --tag k8s-envoylb-goappserver ../
 
 # docker scout cves --type image k8s-envoylb-goappserver
 
 # docker run --name k8s-envoylb-goappserver-c k8s-envoylb-goappserver
 
-docker build -f docker/Dockerfile --tag babbili/k8s-envoylb-goappserver:6aea9c1d .
+cd $DOCKER_DIR && docker build -f Dockerfile --tag babbili/k8s-envoylb-goappserver:6aea9c1d ../
 
 docker scout cves --type image babbili/k8s-envoylb-goappserver:6aea9c1d
 
